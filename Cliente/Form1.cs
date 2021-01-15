7ff65365fc00b5d748b1a98960c5f0996b54d75c
Hugo <hugo3011mendez@gmail.com>
2021-01-15 12:02:03 +0100

    - Añadido código de info, cliente, info del cliente y primeros pasos de las conexión en el servidor.
    - Añadidos todos los componentes necesarios al Form1, el cliente.
    
    - Añadida función de conexión con el par IP-Puerto indicados en el cliente.

--- Cliente/Form1.cs
+++ Cliente/Form1.cs
@@ -7,14 +7,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
 
 namespace Cliente
 {
     public partial class Form1 : Form
     {
+        IPEndPoint ie;
+        Socket server;
+
+        // Función donde el cliente se conecta al servidor indicado
+        public bool conectarServer()
+        {
+            if (txtIP.Text != "" && txtPuerto.Text != "")
+            {
+                // Formateo el texto escrito en los textboxes para la conexión al servidor :
+                ie = new IPEndPoint(IPAddress.Parse(txtIP.Text.Trim()), Convert.ToInt32(txtPuerto.Text.Trim()));
+
+                server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); // Y creo el socket del servidor
+
+                try
+                {
+                    // El cliente inicia la conexión haciendo petición con Connect
+                    server.Connect(ie);
+                }
+                catch (SocketException e)
+                {
+                    Console.WriteLine("Error connection: {0}\nError code: {1}({2})",
+                    e.Message, (SocketError)e.ErrorCode, e.ErrorCode);
+                    Console.ReadKey();
+                    return false; // Si da error en la conexión, devuelve false
+                }
+            }
+            else
+            {
+                return false; // Si los textboxes de la IP y el puerto están vacíos, devuelve false
+            }
+
+            return true;
+        }
+
+
         public Form1()
         {
             InitializeComponent();
         }
     }
+
 }
--- Ejercicio1/Server.cs
+++ Ejercicio1/Server.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
 
 namespace Ejercicio1
 {
@@ -10,7 +13,30 @@ namespace Ejercicio1
     {
         static void Main(string[] args)
         {
+            IPEndPoint ie = new IPEndPoint(IPAddress.Any, 31416); // Establezco un par IP-Puerto para el server
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); // Establezco el Socket
+            s.Bind(ie); // Se enlaza el Socket al IPEndPoint
+            s.Listen(5); // Se queda esperando una conexión y se establece la cola a 5
 
+
+            // Cliente :
+            Socket sClient = s.Accept(); // Aceptamos la conexión del cliente
+
+            // Obtenemos info del cliente :
+            IPEndPoint ieClient = (IPEndPoint)sClient.RemoteEndPoint; // Casteo a IPEndPoint porque EndPoint es más genérico
+            Console.WriteLine("Client connected :\n{0} at port {1}", ieClient.Address, ieClient.Port); // Muestro la IP del cliente y el puerto al que está conectado
+
+            // Conexión :
+            // Uso using para no tener que cerrarlos después
+            using (NetworkStream ns = new NetworkStream(sClient)) // Se crea un Stream que hará de puente entre el Socket, el StreamReader y el StreamWriter
+            using (StreamReader sr = new StreamReader(ns))
+            using (StreamWriter sw = new StreamWriter(ns))
+            {
+                sw.WriteLine("Bienvenido al servidor de hora y fecha!");
+                sw.Flush(); // Fuerzo el envío de los datos sin esperar al cierre de la conexión
+                
+
+            }
         }
     }
 }
